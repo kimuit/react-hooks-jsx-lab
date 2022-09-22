b8b78d79a1e8532df3cbf7f6e234eedaf57aa819
kimuit117 <kemoikiuita@gmail.com>
2022-09-23 00:50:40 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { name, city } from "../data/data.js";
-
+import { image, name, city } from "../data/data.js";
+// import "../index.css"
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="#home">
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+    <p>I love  how to code and actually put it into practice</p>
+    <img src={image} alt='I made this'></img>
+  </div>)
 }
 
 export default Home;
